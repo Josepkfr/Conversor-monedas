543c54ff51da3b54ebee8d2cd152a3181cf98fab
Jose Alpala <alpalajose@gmail.com>
2021-11-24 18:08:56 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dinero from "dinero.js";
+import Rates from "./data/rates";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+Dinero.globalExchangeRatesApi = {
+  currency: "EUR",
+  endpoint: Rates,
+};
+
+const CurrencySelector = ({ rates, title, onChange }) => {
+  return (
+    <div>
+      <p>{title}</p>
+      <select onChange={onChange}>
+        {rates.map((rate, index) => (
+          <option key={index}>{rate} </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 function App() {
+  const [rates, setRates] = useState([]);
+  const [originCurrency, setoriginCurrency] = useState("");
+  const [targetCurrency, settargetCurrency] = useState("");
+  const [originalAmount, setoriginalAmount] = useState([]);
+  const [finalAmount, setfinalAmount] = useState();
+
+  useEffect(() => {
+    let data;
+    Rates.then((value) => {
+      data = value;
+      let ratesArray = Object.keys(data.rates);
+      setRates(ratesArray);
+    });
+  }, []);
+
+  const convert = async () => {
+    let amount = Dinero({
+      amount: parseInt(originalAmount) * 100,
+      currency: originCurrency,
+    });
+    let result = await amount.convert(targetCurrency);
+    setfinalAmount(result.toFormat());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <h1>conversion entre monedas</h1>
+        <div className="flex-container">
+          <CurrencySelector
+            onChange={(e) => setoriginCurrency(e.target.value)}
+            title={`Origen: ${originCurrency}`}
+            rates={rates}
+          />
+          <CurrencySelector
+            onChange={(e) => settargetCurrency(e.target.value)}
+            title={`Destino: ${targetCurrency}`}
+            rates={rates}
+          />
+        </div>
+        <input
+          onChange={(e) => setoriginalAmount(e.target.value)}
+          type="number"
+          className="form-control"
+          placeholder="cantidad en centavos"
+        />
+        {finalAmount && <p>El resultado es: {finalAmount}</p>}
+        {targetCurrency && originCurrency && (
+          <button onClick={convert} className="app-button">
+            Convertir
+          </button>
+        )}
+      </div>
     </div>
   );
 }
